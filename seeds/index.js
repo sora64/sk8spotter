62f3a82ad193544a99587078ca4cccec699d1616
Alan Phelps <phelpsa64@gmail.com>
2022-03-08 18:01:17 -0500

    updated seeds/index.js to require the correct files

--- seeds/index.js
+++ seeds/index.js
@@ -1,6 +1,6 @@
 const sequelize = require('../config/connection');
-const seedSkatepark = require('./galleryData');
-const seedImages = require('./paintingData');
+const seedSkatepark = require('./skateparkData');
+const seedImages = require('./imageData');
 
 const seedAll = async () => {
   await sequelize.sync({ force: true });
