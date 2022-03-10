c6a4b533d0219f4631ef969c069442fb3d236b5b
Alan Phelps <phelpsa64@gmail.com>
2022-03-09 19:20:50 -0500

    image titles, skatepark_id, and filenames input into the seed data

--- seeds/imageData.js
+++ seeds/imageData.js
@@ -2,11 +2,82 @@ const { Image } = require('../models');
 
 const imagedata = [
   {
-    title: 'Blossoming Apricot',
+    title: 'chris',
+    skatepark_id: 2,
+    filename: 'chris-28th.jpg',
+    description:''
+  },
+  {
+    title: 'chris-smith',
+    skatepark_id: 2,
+    filename: 'chris-smith-28th.jpg',
+    description:''
+  },
+  {
+    title: 'dante',
+    skatepark_id: 2,
+    filename: 'dante-28th.jpg',
+    description:''
+  },
+  {
+    title: 'deon',
     skatepark_id: 1,
-    filename: '01-blossoming-apricot.jpg',
-    description:
-      'Branches with pink apricot blossoms against a blue background.',
+    filename: 'deon-texas-beach.jpg',
+    description:''
+  },
+  {
+    title: 'eli',
+    skatepark_id: 1,
+    filename: 'eli-texas-beach.jpg',
+    description:''
+  },
+  {
+    title: 'front-grind',
+    skatepark_id: 1,
+    filename: 'front-grind-texas-beach.jpg',
+    description:''
+  },
+  {
+    title: 'gavin',
+    skatepark_id: 2,
+    filename: 'gavin-28th.jpg',
+    description:''
+  },
+  {
+    title: 'josh',
+    skatepark_id: 3,
+    filename: 'chris-28th.jpg',
+    description:''
+  },
+  {
+    title: 'laurel-bowl',
+    skatepark_id: 3,
+    filename: 'laurel-bowl.jpg',
+    description:''
+  },
+  {
+    title: 'nate-front-board',
+    skatepark_id: 2,
+    filename: 'nate-front-board-28th.png',
+    description:''
+  },
+  {
+    title: 'texas-beach-footplant',
+    skatepark_id: 1,
+    filename: 'texas-beach-footplant.png',
+    description:''
+  },
+  {
+    title: 'turtle-laurel-bowl',
+    skatepark_id: 3,
+    filename: 'turtle-laurel-bowl.png',
+    description:''
+  },
+  {
+    title: 'turtle-vert-laurel',
+    skatepark_id: 3,
+    filename: 'turtle-laurel-bowl.png',
+    description:''
   },
 ];
 
