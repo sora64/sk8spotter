dc05b69171115eeb811d05d3af39e8f8e90af3fd
Alan Phelps <phelpsa64@gmail.com>
2022-03-12 14:26:41 -0500

    played around with 5-star ratings; failed

--- server.js
+++ server.js
@@ -26,7 +26,6 @@ const sess = {
 // Un-comment the following function and insert a new image's appropriate filename between the single quotes to resize that image.
 // resizing('<filename>');
 
-
 app.use(session(sess));
 
 const hbs = exphbs.create({ helpers });
